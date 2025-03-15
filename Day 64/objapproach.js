b6743103581234ac26863e6567a51c76ff40a1ed
Deepak Kumar Mishra <demishra@microsoft.com>
2025-03-15 11:19:11 +0530

    adding code for object mode in Day 64

---
+++ Day 64/objapproach.js
@@ -0,0 +1,43 @@
+obj1={
+
+    1:"Crazy",
+
+    2:"Amazing",
+
+    3:"Fire"
+
+}
+
+obj2={
+
+    1:"Engine",
+
+    2:"Food",
+
+    3:"Garments"
+
+}
+
+obj3={
+
+    1:"Bros",
+
+    2:"Limited",
+
+    3:"Hub"
+
+}
+
+
+
+let num1=Math.floor(Math.random()*3)+1;
+
+// console.log(num1,Math.random()*3);
+
+let num2=Math.floor(Math.random()*3)+1;
+
+let num3=Math.floor(Math.random()*3)+1;
+
+
+
+console.log(obj1[num1]+" "+obj2[num2]+" "+obj3[num3]);
